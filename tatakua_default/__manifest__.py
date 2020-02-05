93e57a0af7fbd5ebb2fcb1ba79466db4b6906fd0
jobiols <jorge.obiols@gmail.com>
2020-02-05 13:37:08 -0300

    inicial

---
+++ tatakua_default/__manifest__.py
@@ -0,0 +1,47 @@
+##############################################################################
+#
+#    Copyright (C) 2019  jeo Software  (http://www.jeosoft.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your optiogitn) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Tatakua',
+    'version': '13.0e.0.0.0',
+    'category': 'Tools',
+    'summary': "Instancia Tatakua",
+    'author': "Tecnopro",
+    'website': 'http://github.com/jobiols/cl-tatakua',
+    'license': 'AGPL-3',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+
+    'port': '8069',
+    'repos': [
+        {'usr': 'jobiols', 'repo': 'cl-tatakua', 'branch': '13.0'},
+        {'usr': 'jobiols', 'repo': 'odoo-paraguay', 'branch': '13.0'},
+    ],
+    'docker': [
+        {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-ent', 'ver': '13.0e'},
+        {'name': 'postgres', 'usr': 'postgres', 'ver': '10.1-alpine'},
+        {'name': 'nginx', 'usr': 'nginx', 'ver': 'latest'},
+    ]
+
+}
